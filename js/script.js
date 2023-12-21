4b22062d929a4b90d0bbc73aafa1f087c85664cc
birds0501 <birds0501@naver.com>
2023-12-21 10:59:25 +0900

    인디케이터 커스텀 설정

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 $(function () {
   //fullpage 초기화
-  $("#fullpage").fullpage();
+  $("#fullpage").fullpage({
+    //1.앵커 설정
+    anchors: ["sec1", "sec2", "sec3", "sec4", "secondPage"],
+    // navigation: true, //인디케이터 우리 거 붙일거임
+
+    //2.menu적용(인디케이터 커스텀)
+    menu: "#menu", //html과 script에 붙여놓고 css로 변경해주기
+  });
 });
